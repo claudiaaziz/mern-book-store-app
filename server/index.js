ea6830c0c692ec04aa67977e1fa5997020103d91
claudiaaziz <claudiahannahh@gmail.com>
2024-07-19 13:22:42 -0400

    Backend setup

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log(req);
+    return res.status(234).send('testingg');
+});
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to db');
+        app.listen(PORT, () => console.log(`App listening on Port: ${PORT}`));
+    })
+    .catch((err) => console.log(err));
